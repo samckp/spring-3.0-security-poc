1c3c372ad2fb04e4aa9b0da6781346236bd79739
samckp <sam.mca.liet@gmail.com>
2023-01-22 20:42:17 +0530

    updated

---
+++ src/main/java/com/spring/security/poc/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.spring.security.poc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/home/public")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+        return httpSecurity.build();
+
+    }
+}
