53670865cc628f3ea03a54eb5a46ae41fd403b48
samckp <sam.mca.liet@gmail.com>
2023-01-24 19:20:15 +0530

    updated

--- src/main/java/com/spring/security/poc/config/SecurityConfig.java
+++ src/main/java/com/spring/security/poc/config/SecurityConfig.java
@@ -3,16 +3,44 @@ package com.spring.security.poc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails normalUser = User.withUsername("sam")
+                .password("{noop}sam123")
+                .roles("NORMAL")
+                .build();
+
+        UserDetails adminUser = User.withUsername("admin")
+                .password("{noop}admin123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.csrf().disable()
                 .authorizeHttpRequests()
+                .requestMatchers("/home/admin")
+                .hasRole("ADMIN")
+                .requestMatchers("/home/normal")
+                .hasRole("NORMAL")
                 .requestMatchers("/home/public")
                 .permitAll()
                 .anyRequest()
